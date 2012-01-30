b9c5bea117f2ff80fe486bcebe5d1ace4e797089
Enrico Franchi <efranchi@ce.unipr.it>
2012-01-30 11:25:35 +0100

    Reformatted a bit.

--- blogracy/src/it/unipr/aotlab/blogracy/web/resolvers/StaticFileResolver.java
+++ blogracy/src/it/unipr/aotlab/blogracy/web/resolvers/StaticFileResolver.java
@@ -38,20 +38,25 @@ import java.io.IOException;
 public class StaticFileResolver implements RequestResolver {
     private String staticFilesDirectory;
 
-    public StaticFileResolver(final String staticFilesDirectory) throws ServerConfigurationError {
+    public StaticFileResolver(final String staticFilesDirectory)
+            throws ServerConfigurationError {
         checksValidStaticRootAndSetField(staticFilesDirectory);
     }
 
     public StaticFileResolver(final String staticFilesDirectory,
-                              final String ignoredPseudoUrl) throws ServerConfigurationError {
-        throw new ServerConfigurationError("Use non capturing regex in the server configuration!");
+                              final String ignoredPseudoUrl)
+            throws ServerConfigurationError {
+        throw new ServerConfigurationError(
+                "Use non capturing regex in the server configuration!"
+        );
     }
 
     /**
      * Checks if {@param staticRoot} exists and is a directory
      *
      * @param staticRoot is the path to check
-     * @throws ServerConfigurationError if {@param staticRoot} does not exist or is not a directory
+     * @throws ServerConfigurationError if {@param staticRoot} does not exist
+     * or is not a directory
      */
     private void checksValidStaticRootAndSetField(final String staticRoot)
             throws ServerConfigurationError {
@@ -74,7 +79,9 @@ public class StaticFileResolver implements RequestResolver {
     }
 
     private static String errorMessageNotDirectory(final String staticRoot) {
-        return "Static files root " + staticRoot + " exists but is not a directory.";
+        return "Static files root "
+                + staticRoot
+                + " exists but is not a directory.";
     }
 
     /**
@@ -85,42 +92,60 @@ public class StaticFileResolver implements RequestResolver {
             final TrackerWebPageRequest request,
             final TrackerWebPageResponse response)
             throws URLMappingError {
-        /*
-        * Keep in mind that due to the underlying TrackerWebPageResponseImpl#useFile(String, String)
-        * implementation, files without extension cannot be processed (why developers assume everybody is using
-        * windows anyway?).
+       /*
+        * Keep in mind that due to the underlying
+        * TrackerWebPageResponseImpl#useFile(String, String)
+        * implementation, files without extension cannot be processed
+        * (why developers assume everybody is using windows anyway?).
         *
-        * If someone feels a compelling reason to resolve a file without extension, he may wrote some crazy code
-        * to run around the default implementation of TrackerWebPageResponseImpl, like copy the file to a file with
-        * extension and  send that. Then they could go and meet the original developers with an {@code Object}
-        * implementing  the {@code Bludgeoning} interface and repeatedly call {@code Bludgeoning#hitOnTheHead} passing
-        * the aforementioned developers as actual parameters.
+        * If someone feels a compelling reason to resolve a file without
+        * extension, he may wrote some crazy code to run around the
+        * default implementation of TrackerWebPageResponseImpl, like
+        * copy the file to a file with extension and  send that. Then
+        * they could go and meet the original developers with an {@code
+        * Object} implementing  the {@code Bludgeoning} interface and
+        * repeatedly call {@code Bludgeoning#hitOnTheHead} passing the
+        * aforementioned developers as actual parameters.
         *
-        * Ah, when there is no doc, we are always obliged to read the implementations. So perhaps some day this will
-        * not be true anymore.
+        * Ah, when there is no doc, we are always obliged to read the
+        * implementations. So perhaps some day this will not be true
+        * anymore.
         *
-        * Note to self: when you suppose someone else is reading your code and you are doing
-        * some low level stuff like manually checking if a file has an extension, do not use explicative names like
-        * {@code fileHasExtension(filename)} and similar over-engineering techniques: since your code is worth being read only by
-        * true hackers, rather leave the low level stuff and use an equivalent 6502 asm program to explain your goals.
+        * Note to self: when you suppose someone else is reading your
+        * code and you are doing some low level stuff like manually
+        * checking if a file has an extension, do not use explicative
+        * names like {@code fileHasExtension(filename)} and similar
+        * over-engineering techniques: since your code is worth being
+        * read only by true hackers, rather leave the low level stuff
+        * and use an equivalent 6502 asm program to explain your goals.
         *
-        * Besides, actually TrackerWebPageResponseImpl#useFile(String, String)
-        * does set the right {@code Content-Type} headers. Well, <i>right</i> according to the hash table
-        * hardcoded in {@link com.aelitis.azureus.core.util.HTTPUtils}. If your file do not fit I would just
-        * suggest the {@code Bludgeoning} trick once again. My whole idea of having a pluggable mime-type resolver
-        * is just for fools who think there are more than 27 or so different file-types.
+        * Besides, actually TrackerWebPageResponseImpl#useFile(String,
+        * String) does set the right {@code Content-Type} headers. Well,
+        * <i>right</i> according to the hash table hardcoded in {@link
+        * com.aelitis.azureus.core.util.HTTPUtils}. If your file do not
+        * fit I would just suggest the {@code Bludgeoning} trick once
+        * again. My whole idea of having a pluggable mime-type resolver
+        * is just for fools who think there are more than 27 or so
+        * different file-types.
         *
-        * Since {@link com.aelitis.azureus.core.util.HTTPUtils#guessContentTypeFromFileType(String)} returns
-        * the argument if there is no match and that is what is called by
-        * {@link org.gudy.azureus2.pluginsimpl.local.tracker.TrackerWebPageResponseImpl#useStream(String, java.io.InputStream)}
-        * to resolve the first String parameter, you may want to use a pluggable mime-type resolver with the "right"
-        * mime-type (which being no extension will be used as the true content type). Have bloody fun with it.
+        * Since {@link
+        * com.aelitis.azureus.core.util.HTTPUtils#guessContentTypeFromFileType(String)}
+        * returns the argument if there is no match and that is what is
+        * called by {@link
+        * org.gudy.azureus2.pluginsimpl.local.tracker.TrackerWebPageResponseImpl#useStream(String,
+        * java.io.InputStream)} to resolve the first String parameter,
+        * you may want to use a pluggable mime-type resolver with the
+        * "right" mime-type (which being no extension will be used as
+        * the true content type). Have bloody fun with it.
         */
 
         final String url = request.getURL();
 
         try {
-            boolean didSendTheFile = response.useFile(staticFilesDirectory, url);
+            boolean didSendTheFile = response.useFile(
+                    staticFilesDirectory,
+                    url
+            );
             if (!didSendTheFile) {
                 if (mayBeDirectoryUrl(url)) {
                     redirectToIndexInDirectory(url, response);
@@ -142,7 +167,8 @@ public class StaticFileResolver implements RequestResolver {
 
     }
 
-    private void redirectToIndexInDirectory(final String url, final TrackerWebPageResponse response) {
+    private void redirectToIndexInDirectory(
+            final String url, final TrackerWebPageResponse response) {
         Logger.info(url + " may be a directory. Trying to send index.html instead.");
         response.setReplyStatus(HttpResponseCode.HTTP_SEE_OTHER);
         response.setHeader("Location", url + "index.html");
