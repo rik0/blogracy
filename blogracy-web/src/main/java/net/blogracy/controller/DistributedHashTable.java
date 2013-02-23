a7c97933b3305b98299a361d55fa61e3bd9c5379
mic <mic@mic-Studio-XPS-1340.(none)>
2013-02-23 14:30:14 +0100

    update the records file aftera seed is actually received

--- blogracy-web/src/main/java/net/blogracy/controller/DistributedHashTable.java
+++ blogracy-web/src/main/java/net/blogracy/controller/DistributedHashTable.java
@@ -67,165 +67,170 @@ import com.google.inject.name.Names;
  */
 public class DistributedHashTable {
 
-	private ConnectionFactory connectionFactory;
-	private Connection connection;
-	private Session session;
-	private Destination lookupQueue;
-	private Destination storeQueue;
-	private Destination downloadQueue;
-	private MessageProducer producer;
-	private MessageConsumer consumer;
+    private ConnectionFactory connectionFactory;
+    private Connection connection;
+    private Session session;
+    private Destination lookupQueue;
+    private Destination storeQueue;
+    private Destination downloadQueue;
+    private MessageProducer producer;
+    private MessageConsumer consumer;
 
-	static final String CACHE_FOLDER = Configurations.getPathConfig()
-			.getCachedFilesDirectoryPath();
+    static final String CACHE_FOLDER = Configurations.getPathConfig()
+            .getCachedFilesDirectoryPath();
 
-	private HashMap<String, JSONObject> records = new HashMap<String, JSONObject>();
+    private HashMap<String, JSONObject> records = new HashMap<String, JSONObject>();
 
-	private static final DistributedHashTable THE_INSTANCE = new DistributedHashTable();
+    private static final DistributedHashTable THE_INSTANCE = new DistributedHashTable();
 
-	public static DistributedHashTable getSingleton() {
-		return THE_INSTANCE;
-	}
+    public static DistributedHashTable getSingleton() {
+        return THE_INSTANCE;
+    }
 
-	public DistributedHashTable() {
-		try {
-			File recordsFile = new File(CACHE_FOLDER + File.separator
-					+ "records.json");
-			if (recordsFile.exists()) {
-				JSONArray recordList = new JSONArray(new JSONTokener(
-						new FileReader(recordsFile)));
-				for (int i = 0; i < recordList.length(); ++i) {
-					JSONObject record = recordList.getJSONObject(i);
-					records.put(record.getString("id"), record);
-				}
-			}
+    public DistributedHashTable() {
+        try {
+            File recordsFile = new File(CACHE_FOLDER + File.separator
+                    + "records.json");
+            if (recordsFile.exists()) {
+                JSONArray recordList = new JSONArray(new JSONTokener(
+                        new FileReader(recordsFile)));
+                for (int i = 0; i < recordList.length(); ++i) {
+                    JSONObject record = recordList.getJSONObject(i);
+                    records.put(record.getString("id"), record);
+                }
+            }
 
-			connectionFactory = new ActiveMQConnectionFactory(
-					ActiveMQConnection.DEFAULT_BROKER_URL);
-			connection = connectionFactory.createConnection();
-			connection.start();
-			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-			producer = session.createProducer(null);
-			producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-			lookupQueue = session.createQueue("lookup");
-			storeQueue = session.createQueue("store");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            connectionFactory = new ActiveMQConnectionFactory(
+                    ActiveMQConnection.DEFAULT_BROKER_URL);
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            producer = session.createProducer(null);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+            lookupQueue = session.createQueue("lookup");
+            storeQueue = session.createQueue("store");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void lookup(final String id) {
-		try {
-			Destination tempDest = session.createTemporaryQueue();
-			MessageConsumer responseConsumer = session.createConsumer(tempDest);
-			responseConsumer.setMessageListener(new MessageListener() {
-				@Override
-				public void onMessage(Message response) {
-					try {
-						String msgText = ((TextMessage) response).getText();
-						JSONObject keyValue = new JSONObject(msgText);
-						String value = keyValue.getString("value");
-						PublicKey signerKey = JsonWebSignature
-								.getSignerKey(value);
-						JSONObject record = new JSONObject(JsonWebSignature
-								.verify(value, signerKey));
-						JSONObject currentRecord = getRecord(id);
-						if (currentRecord == null
-								|| currentRecord.getString("version")
-										.compareTo(record.getString("version")) < 0) {
-							putRecord(record);
-							String uri = record.getString("uri");
-							FileSharing.getSingleton().download(uri, ".json");
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});
+    public void lookup(final String id) {
+        try {
+            Destination tempDest = session.createTemporaryQueue();
+            MessageConsumer responseConsumer = session.createConsumer(tempDest);
+            responseConsumer.setMessageListener(new MessageListener() {
+                @Override
+                public void onMessage(Message response) {
+                    try {
+                        String msgText = ((TextMessage) response).getText();
+                        JSONObject keyValue = new JSONObject(msgText);
+                        String value = keyValue.getString("value");
+                        PublicKey signerKey = JsonWebSignature
+                                .getSignerKey(value);
+                        final JSONObject record = new JSONObject(JsonWebSignature
+                                .verify(value, signerKey));
+                        JSONObject currentRecord = getRecord(id);
+                        if (currentRecord == null
+                                || currentRecord.getString("version")
+                                        .compareTo(record.getString("version")) < 0) {
+                            String uri = record.getString("uri");
+                            FileSharing fileSharing = FileSharing.getSingleton();
+                            String hash = fileSharing.getHashFromMagnetURI(uri);
+                            fileSharing.downloadByHash(hash, ".json", new MessageListener() {
+                                public void onMessage(Message response) {
+                                    putRecord(record);
+                                }
+                            });
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
-			JSONObject record = new JSONObject();
-			record.put("id", id);
+            JSONObject record = new JSONObject();
+            record.put("id", id);
 
-			TextMessage message = session.createTextMessage();
-			message.setText(record.toString());
-			message.setJMSReplyTo(tempDest);
-			producer.send(lookupQueue, message);
+            TextMessage message = session.createTextMessage();
+            message.setText(record.toString());
+            message.setJMSReplyTo(tempDest);
+            producer.send(lookupQueue, message);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void store(final String id, final String uri, final String version) {
-		this.store(id, uri, version, null, null);
-	}
+    public void store(final String id, final String uri, final String version) {
+        this.store(id, uri, version, null, null);
+    }
 
-	public void store(final String id, final String uri, final String version,
-			final JSONArray albums, final JSONArray mediaItems) {
-		try {
-			JSONObject record = new JSONObject();
-			record.put("id", id);
-			record.put("uri", uri);
-			record.put("version", version);
-			// put "magic" public-key; e.g.
-			// RSA.modulus(n).exponent(e)
-			// record.put("signature", user); // TODO
+    public void store(final String id, final String uri, final String version,
+            final JSONArray albums, final JSONArray mediaItems) {
+        try {
+            JSONObject record = new JSONObject();
+            record.put("id", id);
+            record.put("uri", uri);
+            record.put("version", version);
+            // put "magic" public-key; e.g.
+            // RSA.modulus(n).exponent(e)
+            // record.put("signature", user); // TODO
 
-			if (albums != null && albums.length() > 0) {
-				record.put("albums", albums);
-			}
+            if (albums != null && albums.length() > 0) {
+                record.put("albums", albums);
+            }
 
-			if (mediaItems != null && mediaItems.length() > 0) {
-				record.put("mediaItems", mediaItems);
-			}
+            if (mediaItems != null && mediaItems.length() > 0) {
+                record.put("mediaItems", mediaItems);
+            }
 
-			KeyPair keyPair = Configurations.getUserConfig().getUserKeyPair();
-			String value = JsonWebSignature.sign(record.toString(), keyPair);
+            KeyPair keyPair = Configurations.getUserConfig().getUserKeyPair();
+            String value = JsonWebSignature.sign(record.toString(), keyPair);
 
-			JSONObject keyValue = new JSONObject();
-			keyValue.put("key", id);
-			keyValue.put("value", value);
-			TextMessage message = session.createTextMessage();
-			message.setText(keyValue.toString());
-			producer.send(storeQueue, message);
-			putRecord(record);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            JSONObject keyValue = new JSONObject();
+            keyValue.put("key", id);
+            keyValue.put("value", value);
+            TextMessage message = session.createTextMessage();
+            message.setText(keyValue.toString());
+            producer.send(storeQueue, message);
+            putRecord(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public JSONObject getRecord(String user) {
-		return records.get(user);
-	}
+    public JSONObject getRecord(String user) {
+        return records.get(user);
+    }
 
-	public void putRecord(JSONObject record) {
-		try {
-		  String id = record.getString("id");
-		  JSONObject old = records.get(id);
-		  if (old != null) {
-		    String prev = old.getString("uri");
-		    record.put("prev", prev);
-		  }
-			records.put(id, record);
-		} catch (JSONException e1) {
-			e1.printStackTrace();
-		}
-		JSONArray recordList = new JSONArray();
-		Iterator<JSONObject> entries = records.values().iterator();
-		while (entries.hasNext()) {
-			JSONObject entry = entries.next();
-			recordList.put(entry);
-		}
-		File recordsFile = new File(CACHE_FOLDER + File.separator
-				+ "records.json");
-		try {
-			FileWriter writer = new FileWriter(recordsFile);
-			recordList.write(writer);
-			writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-	}
+    public void putRecord(JSONObject record) {
+        try {
+          String id = record.getString("id");
+          JSONObject old = records.get(id);
+          if (old != null) {
+            String prev = old.getString("uri");
+            record.put("prev", prev);
+          }
+            records.put(id, record);
+        } catch (JSONException e1) {
+            e1.printStackTrace();
+        }
+        JSONArray recordList = new JSONArray();
+        Iterator<JSONObject> entries = records.values().iterator();
+        while (entries.hasNext()) {
+            JSONObject entry = entries.next();
+            recordList.put(entry);
+        }
+        File recordsFile = new File(CACHE_FOLDER + File.separator
+                + "records.json");
+        try {
+            FileWriter writer = new FileWriter(recordsFile);
+            recordList.write(writer);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
