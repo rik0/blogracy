ceddd2037930965d081443df21f7125d67481b72
Marco <bigno.bibbs@gmail.com>
2014-10-03 10:45:38 +0200

    Indentation Correction

--- blogracy-web/src/test/java/net/blogracy/util/CpAbeTest.java
+++ blogracy-web/src/test/java/net/blogracy/util/CpAbeTest.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.assertEquals;
 public class CpAbeTest {
 	
 	private static final CpAbeController cpabe = CpAbeController.getSingleton();
-    private static final FileSharing sharing = FileSharing.getSingleton();
+	private static final FileSharing sharing = FileSharing.getSingleton();
 
 	private static String CPABE = "CpAbeTest-";
 	private static String ATTRIBUTE = "role:student class:B";
@@ -41,53 +41,53 @@ public class CpAbeTest {
 	private static String helloWorld = "Hello World\n";
 	
 	@BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-		cpabe.setup();
-		cpabe.setFriendPrivateKey(Configurations.getUserConfig().getUser().getHash().toString(), ATTRIBUTE);
+	public static void setUpBeforeClass() throws Exception {
+	    cpabe.setup();
+	    cpabe.setFriendPrivateKey(Configurations.getUserConfig().getUser().getHash().toString(), ATTRIBUTE);
 		
-		message = testFolder.newFile("cache" + File.separator + MESSAGE);
-		org.apache.commons.io.FileUtils.writeStringToFile(message, helloWorld);
+	    message = testFolder.newFile("cache" + File.separator + MESSAGE);
+	    org.apache.commons.io.FileUtils.writeStringToFile(message, helloWorld);
 		
-		String uriCipherInfo = cpabe.getUriCipherInfo();
-		cipherInfoJSON = FileUtils.getJSONFromFile(new File("cache" + File.separator
+	    String uriCipherInfo = cpabe.getUriCipherInfo();
+	    cipherInfoJSON = FileUtils.getJSONFromFile(new File("cache" + File.separator
 				+ sharing.getHashFromMagnetURI(uriCipherInfo) + ".json"));
 		
-		publicKeyFile = testFolder.newFile(cpabe.getPubPath());
-		org.apache.commons.io.FileUtils.writeStringToFile(publicKeyFile, cipherInfoJSON.get("pubkey").toString() );
+	    publicKeyFile = testFolder.newFile(cpabe.getPubPath());
+	    org.apache.commons.io.FileUtils.writeStringToFile(publicKeyFile, cipherInfoJSON.get("pubkey").toString() );
 
-		privateKeyFile = testFolder.newFile("cache" + File.separator + PRVFILE );
-		//org.apache.commons.io.FileUtils.writeStringToFile(privatekeyFile, );
+	    privateKeyFile = testFolder.newFile("cache" + File.separator + PRVFILE );
+	    //org.apache.commons.io.FileUtils.writeStringToFile(privatekeyFile, );
 		
-		decFile = testFolder.newFile("cache" + File.separator + DECFILE);
-    }
+	    decFile = testFolder.newFile("cache" + File.separator + DECFILE);
+	}
 
-    @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    }
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 
-    @Before
-    public void setUp() throws Exception {
-    }
+	@Before
+	public void setUp() throws Exception {
+	}
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@After
+	public void tearDown() throws Exception {
+	}
 
 
-    @Test
-    public void testEncDecrypt() throws Exception {
-    	System.out.println("Cifro...");
-    	cpabe.encryptMessage(publicKeyFile.getPath().toString(), 
-    			POLICY, 
-    			message.getPath().toString(),
-    			"cache" + File.separator + ENCFILE);
-    	System.out.println("Decifro...");
-    	cpabe.decryptMessage(publicKeyFile.getPath().toString(),
-    			             cpabe.getPrivateKeyFile(cipherInfoJSON).getPath().toString(),
-    						 "cache" + File.separator + ENCFILE,
-    						 decFile.getPath().toString());
-    	System.out.println("Confronto...");
-    	assertEquals(helloWorld, FileUtils.getContentFromFile(decFile).toString());
-    }
+	@Test
+	public void testEncDecrypt() throws Exception {
+    	    System.out.println("Cifro...");
+    	    cpabe.encryptMessage(publicKeyFile.getPath().toString(), 
+    				POLICY, 
+    				message.getPath().toString(),
+    				"cache" + File.separator + ENCFILE);
+    	    System.out.println("Decifro...");
+    	    cpabe.decryptMessage(publicKeyFile.getPath().toString(),
+    			         cpabe.getPrivateKeyFile(cipherInfoJSON).getPath().toString(),
+    				 "cache" + File.separator + ENCFILE,
+    				 decFile.getPath().toString());
+    	    System.out.println("Confronto...");
+    	    assertEquals(helloWorld, FileUtils.getContentFromFile(decFile).toString());
+	}
     
 }
